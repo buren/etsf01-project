6f023cbb64d6d8f8b1c70d9b1db80c99a6d76bad
buren <burenstam@gmail.com>
2012-03-22 13:16:40 +0100

    added github guide

---
+++ tools/Github quick guide.sh
@@ -0,0 +1,23 @@
+# Git need to know
+
+
+# Update from master repository
+$ git pull git@github.com:buren/etsf01-project
+
+# Add one file to next commit 
+$ git add PATH_TO_FILE
+
+# Add all files to next commit
+$ git add .
+
+# Commit local changes
+$ git commit -m "Commit message"
+
+# Push changes to your forked repository
+$ git push origin master
+
+# To merge your commit to the master repository 
+# 1) Go to github.com and to your forked repository
+# 2) Click on "Pull request"
+# 3) After that github will re-route you to the master repository (buren/etsf01-project)
+# 4) Click on "Merge" to merge your changes
